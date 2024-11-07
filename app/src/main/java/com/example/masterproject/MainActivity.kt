4136a4d72897ea969a0decde55bf81ded3384344
zyeds <zyed.star@gmail.com>
2024-11-07 13:05:10 +0200

    last comit

--- app/src/main/java/com/example/masterproject/MainActivity.kt
+++ app/src/main/java/com/example/masterproject/MainActivity.kt
@@ -1,47 +1,104 @@
 package com.example.masterproject
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.masterproject.ui.theme.MasterProjectTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MasterProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
         }
     }
 }
 
+@Preview(showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen(){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MasterProjectTheme {
-        Greeting("Android")
+    ) {
+        Text(
+            text = stringResource(R.string.header_app),
+            fontSize = 25.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 200.dp)
+        )
+        Text(
+            text = stringResource(R.string.exchange_rate),
+            fontSize = 25.sp,
+            textDecoration = TextDecoration.Underline,
+            modifier = Modifier.padding(30.dp)
+                .clickable(
+                    onClick = {
+
+                    },
+                    interactionSource = MutableInteractionSource(),
+                    indication = rememberRipple()
+                )
+        )
+        Text(
+            text = stringResource(R.string.forecast_from_gpt),
+            fontSize = 25.sp,
+            textDecoration = TextDecoration.Underline,
+            modifier = Modifier.padding(30.dp)
+                .clickable(
+                    onClick = {
+
+                    },
+                    interactionSource = MutableInteractionSource(),
+                    indication = rememberRipple()
+                )
+        )
+        Text(
+            text = stringResource(R.string.data_store),
+            fontSize = 25.sp,
+            textDecoration = TextDecoration.Underline,
+            modifier = Modifier.padding(30.dp)
+                .clickable(
+                    onClick = {
+
+                    },
+                    interactionSource = MutableInteractionSource(),
+                    indication = rememberRipple()
+                )
+        )
+        Text(
+            text = stringResource(R.string.setting),
+            fontSize = 25.sp,
+            textDecoration = TextDecoration.Underline,
+            modifier = Modifier.padding(30.dp)
+                .clickable(
+                    onClick = {
+
+                    },
+                    interactionSource = MutableInteractionSource(),
+                    indication = rememberRipple()
+                )
+        )
     }
 }
--- app/src/main/java/com/example/masterproject/ui/theme/Theme.kt
+++ app/src/main/java/com/example/masterproject/ui/theme/Theme.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.sp
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
