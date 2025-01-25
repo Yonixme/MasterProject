33baebf22e6bc82c9d697549f8c989661bb4b26f
zyeds <zyed.star@gmail.com>
2025-01-25 17:53:38 +0200

    updated icons

--- app/src/main/java/com/example/masterproject/ui/screens/AppTab.kt
+++ app/src/main/java/com/example/masterproject/ui/screens/AppTab.kt
@@ -13,12 +13,12 @@ data class AppTab(
 
 val MainTabs = persistentListOf(
     AppTab(
-        iconRes = R.drawable.home_icon,
+        iconRes = R.drawable.home_icon_vector,
         labelRes = R.string.main_screen,
         graph = MainGraph
     ),
     AppTab(
-        iconRes = R.drawable.trade_icon,
+        iconRes = R.drawable.histogram_icon,
         labelRes = R.string.exchange_rate_short,
         graph = ExchangeRateGraphs
     ),
